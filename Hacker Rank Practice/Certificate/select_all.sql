c46157c1772a774d692dc3aa74d701903766a132
Abhay Vishe <141409692+Abhayvishe@users.noreply.github.com>
2026-01-08 12:37:10 +0530

    Add comments and additional queries to select_all.sql

--- Hacker Rank Practice/Certificate/select_all.sql
+++ Hacker Rank Practice/Certificate/select_all.sql
@@ -2,12 +2,12 @@
 -- Platform: HackerRank
 -- Difficulty: Easy
 -- Topic: SQL Basics
-
 SELECT NAME
 FROM CITY
 WHERE CountryCode = 'USA'
   AND Population > 120000
 ORDER BY NAME DESC;
+
 
 
 -- Problem: Query all columns (attributes) for every row in the CITY table.
@@ -18,12 +18,32 @@ ORDER BY NAME DESC;
 SELECT *
 FROM CITY;
 
+
+
+
 -- Problem: Query all columns for a city in CITY with the ID 1661
 -- Platform: HackerRank
 -- Difficulty: Easy
 -- Topic: SQL Basics
-
 SELECT *
 FROM CITY
 WHERE ID = 1661;
 
+
+
+-- Problem: Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Topic: SQL Basics
+SELECT *
+FROM CITY
+WHERE CountryCode = 'JPN';
+
+
+
+-- Problem: Query a list of CITY and STATE from the STATION table
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Topic: SQL Basics
+SELECT CITY, STATE
+FROM STATION;
