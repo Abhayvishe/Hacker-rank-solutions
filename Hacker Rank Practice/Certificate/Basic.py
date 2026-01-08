bc1e2ffd58e8a9706bbcb6f1cea51419f8caa5e1
Abhay Vishe <141409692+Abhayvishe@users.noreply.github.com>
2026-01-08 12:44:19 +0530

    Update Basic.py

--- Hacker Rank Practice/Certificate/Basic.py
+++ Hacker Rank Practice/Certificate/Basic.py
@@ -1,1 +1,8 @@
+-- Problem: Say Hello, World! With Python
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Topic:Pyton Basics
+
+Submitted Code = my_string = "Hello, World!"
+                  print(my_string)
 
