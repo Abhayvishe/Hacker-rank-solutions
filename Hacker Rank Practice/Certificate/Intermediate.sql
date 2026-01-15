9dad69c1d9fa004209294221f4eb709214286cc9
Abhay Vishe <141409692+Abhayvishe@users.noreply.github.com>
2026-01-15 23:34:26 +0530

    Add queries for shortest and longest city names
    Add SQL queries to find cities with shortest and longest names.

--- Hacker Rank Practice/Certificate/Intermediate.sql
+++ Hacker Rank Practice/Certificate/Intermediate.sql
@@ -1,1 +1,7 @@
+-- Problem: Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+-- Platform: HackerRank
+-- Difficulty: Intermediate
+-- Topic: SQL Basics
+SELECT CITY, LENGTH(CITY) as l FROM STATION ORDER BY l ASC, CITY LIMIT 1;
 
+SELECT CITY, LENGTH(CITY) as l FROM STATION ORDER BY  l DESC, CITY LIMIT 1;
